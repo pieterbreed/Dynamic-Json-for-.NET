b8d1fd2a89e9b8ed79efc7a777de83217593fe09
Pieter Breed <pieter.breed@allangray.co.za>
2013-07-17 14:00:16 +0200

    bumping nuget versions

--- DynamicJson/Properties/AssemblyInfo.cs
+++ DynamicJson/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pieter Breed")]
 [assembly: AssemblyProduct("DynamicJson")]
-[assembly: AssemblyCopyright("Copyright © Pieter Breed 2011")]
+[assembly: AssemblyCopyright("Copyright © Pieter Breed 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.7.0")]
-[assembly: AssemblyFileVersion("0.9.7.0")]
+[assembly: AssemblyVersion("0.9.8.0")]
+[assembly: AssemblyFileVersion("0.9.8.0")]
